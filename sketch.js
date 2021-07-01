7380ea8ab5e49f065ee1c195837286e29c8b1052
Neha2468 <81560725+Neha2468@users.noreply.github.com>
2021-07-01 22:43:21 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper;
+var wall1,wall2,wall3;
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(100, 600, 10);
+
+	ground = new Ground(400,680,800,10);
+
+
+	wall1 = new Dustbin(550,613,20,100);
+	wall2 = new Dustbin(635,660,150,20);
+	wall3 = new Dustbin(720,613,20,100);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  
+  paper.display();
+
+  wall1.display();
+  wall2.display();
+  wall3.display();
+
+  ground.display();
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.body,paper.body.position, {x:15,y:-15} );
+	}
+}
